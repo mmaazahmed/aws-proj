4b910b5e7a4359d9db4f6be377e8bd1aa99e89f7
mmaazahmed <mmaazahmedpoke@gmail.com>
2024-03-20 11:25:14 +0500

    fixed vpc

---
+++ productApp/webpack.config.js
@@ -0,0 +1,24 @@
+const nodeExternals=require('webpack-node-externals');
+const CopyPlugin = require('copy-webpack-plugin');
+
+
+module.exports={
+    target:'node',
+    mode: 'none',
+    externals: [nodeExternals()],
+
+    plugins:[
+        new CopyPlugin({
+            patterns: [
+              { from: './node_modules/.prisma/client/schema.prisma', to: './' }, // you may need to change `to` here.
+            ],
+          }),
+    ],
+    // plugins: [
+    //   new CopyPlugin({ patterns: ['./node_modules/.prisma/client/schema.prisma'] }), // without this the prisma generate above will not work
+    // ],
+    resolve: {
+        extensions: ['.js', '.json'], 
+    },
+    
+};
